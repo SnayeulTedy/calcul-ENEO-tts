bcb10e094640c95708368e03aee826895af7e1d3
SnayeulTedy <161803875+SnayeulTedy@users.noreply.github.com>
2024-05-11 16:59:00 +0100

    Add files via upload
    By MISTER~S, Ted's SNAYEUL

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace ENEO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtConsommation = new System.Windows.Forms.TextBox();
+            this.txtQuartier = new System.Windows.Forms.TextBox();
+            this.txtVille = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbMT = new System.Windows.Forms.RadioButton();
+            this.rbBT = new System.Windows.Forms.RadioButton();
+            this.btnCalculer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnEnregistrer = new System.Windows.Forms.Button();
+            this.btnCharger = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rtbFacture = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNom);
+            this.groupBox1.Controls.Add(this.txtConsommation);
+            this.groupBox1.Controls.Add(this.txtQuartier);
+            this.groupBox1.Controls.Add(this.txtVille);
+            this.groupBox1.Controls.Add(this.txtPrenom);
+            this.groupBox1.Controls.Add(this.txtIdentifiant);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 254);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "INFORMATIONS DU CLIENT";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNom.Location = new System.Drawing.Point(211, 62);
+            this.txtNom.Multiline = true;
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(208, 31);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtConsommation
+            // 
+            this.txtConsommation.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsommation.Location = new System.Drawing.Point(211, 209);
+            this.txtConsommation.Multiline = true;
+            this.txtConsommation.Name = "txtConsommation";
+            this.txtConsommation.Size = new System.Drawing.Size(208, 31);
+            this.txtConsommation.TabIndex = 1;
+            // 
+            // txtQuartier
+            // 
+            this.txtQuartier.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuartier.Location = new System.Drawing.Point(211, 172);
+            this.txtQuartier.Multiline = true;
+            this.txtQuartier.Name = "txtQuartier";
+            this.txtQuartier.Size = new System.Drawing.Size(208, 31);
+            this.txtQuartier.TabIndex = 1;
+            // 
+            // txtVille
+            // 
+            this.txtVille.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVille.Location = new System.Drawing.Point(211, 135);
+            this.txtVille.Multiline = true;
+            this.txtVille.Name = "txtVille";
+            this.txtVille.Size = new System.Drawing.Size(208, 31);
+            this.txtVille.TabIndex = 1;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrenom.Location = new System.Drawing.Point(211, 99);
+            this.txtPrenom.Multiline = true;
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(208, 31);
+            this.txtPrenom.TabIndex = 1;
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.Location = new System.Drawing.Point(211, 21);
+            this.txtIdentifiant.Multiline = true;
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(91, 35);
+            this.txtIdentifiant.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(55, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 19);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "CONSO DU MOIS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(108, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "QUARTIER";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(146, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "VILLE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(121, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "PRENOM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "NOM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbMT);
+            this.groupBox2.Controls.Add(this.rbBT);
+            this.groupBox2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(36, 292);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(426, 109);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TYPE DE RESEAU";
+            // 
+            // rbMT
+            // 
+            this.rbMT.AutoSize = true;
+            this.rbMT.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMT.Location = new System.Drawing.Point(12, 72);
+            this.rbMT.Name = "rbMT";
+            this.rbMT.Size = new System.Drawing.Size(242, 23);
+            this.rbMT.TabIndex = 1;
+            this.rbMT.TabStop = true;
+            this.rbMT.Text = "MOYENNE TENSION (MT)";
+            this.rbMT.UseVisualStyleBackColor = true;
+            // 
+            // rbBT
+            // 
+            this.rbBT.AutoSize = true;
+            this.rbBT.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbBT.Location = new System.Drawing.Point(12, 37);
+            this.rbBT.Name = "rbBT";
+            this.rbBT.Size = new System.Drawing.Size(197, 23);
+            this.rbBT.TabIndex = 0;
+            this.rbBT.TabStop = true;
+            this.rbBT.Text = "BASSE TENSION (BT)";
+            this.rbBT.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculer
+            // 
+            this.btnCalculer.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculer.Location = new System.Drawing.Point(48, 423);
+            this.btnCalculer.Name = "btnCalculer";
+            this.btnCalculer.Size = new System.Drawing.Size(95, 30);
+            this.btnCalculer.TabIndex = 2;
+            this.btnCalculer.Text = "Calculer";
+            this.btnCalculer.UseVisualStyleBackColor = true;
+            this.btnCalculer.Click += new System.EventHandler(this.btnCalculer_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModifier.Location = new System.Drawing.Point(147, 423);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(95, 30);
+            this.btnModifier.TabIndex = 2;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnEnregistrer
+            // 
+            this.btnEnregistrer.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnregistrer.Location = new System.Drawing.Point(248, 423);
+            this.btnEnregistrer.Name = "btnEnregistrer";
+            this.btnEnregistrer.Size = new System.Drawing.Size(113, 30);
+            this.btnEnregistrer.TabIndex = 2;
+            this.btnEnregistrer.Text = "Enregistrer";
+            this.btnEnregistrer.UseVisualStyleBackColor = true;
+            this.btnEnregistrer.Click += new System.EventHandler(this.btnEnregistrer_Click);
+            // 
+            // btnCharger
+            // 
+            this.btnCharger.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCharger.Location = new System.Drawing.Point(367, 422);
+            this.btnCharger.Name = "btnCharger";
+            this.btnCharger.Size = new System.Drawing.Size(95, 30);
+            this.btnCharger.TabIndex = 2;
+            this.btnCharger.Text = "Charger";
+            this.btnCharger.UseVisualStyleBackColor = true;
+            this.btnCharger.Click += new System.EventHandler(this.btnCharger_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(485, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 19);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "FACTURE DU MOIS";
+            // 
+            // rtbFacture
+            // 
+            this.rtbFacture.Location = new System.Drawing.Point(488, 47);
+            this.rtbFacture.Name = "rtbFacture";
+            this.rtbFacture.Size = new System.Drawing.Size(437, 399);
+            this.rtbFacture.TabIndex = 4;
+            this.rtbFacture.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(964, 491);
+            this.Controls.Add(this.rtbFacture);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCharger);
+            this.Controls.Add(this.btnEnregistrer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnCalculer);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtConsommation;
+        private System.Windows.Forms.TextBox txtQuartier;
+        private System.Windows.Forms.TextBox txtVille;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtIdentifiant;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbMT;
+        private System.Windows.Forms.RadioButton rbBT;
+        private System.Windows.Forms.Button btnCalculer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnEnregistrer;
+        private System.Windows.Forms.Button btnCharger;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox rtbFacture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ENEO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Methodes de calcul de facture
+
+        private double CalculerFactureBT(int consommation)
+        {
+            if (consommation <= 110)
+                return consommation * 50;
+            else if (consommation <= 400)
+                return (110 * 50) + ((consommation - 110) * 79);
+            else if (consommation <= 800)
+                return (110 * 50) + (290 * 79) + ((consommation - 400) * 94);
+            else
+                return (110 * 50) + (290 * 79) + (400 * 94) + ((consommation - 800) * 99);
+        }
+
+        private double CalculerFactureMT(int consommation)
+        {
+            double primeFixe = 3700;
+            double tarifparkwh = 150;
+            double total = primeFixe + (tarifparkwh * consommation);
+            return total;
+        }
+        #endregion
+
+
+        #region Evenements et Controles
+
+        private void btnCalculer_Click(object sender, EventArgs e)
+        {
+            string nomClient = txtNom.Text;
+            string prenomClient = txtPrenom.Text;
+            string villeClient = txtVille.Text;
+            string quartierClient = txtQuartier.Text;
+            string identifiantClient = txtIdentifiant.Text;
+            int consommation;
+            double montantTotal;
+
+            if(int.TryParse(txtConsommation.Text, out consommation) && consommation >= 0)
+            {
+                if(rbBT.Checked)
+                {
+                    montantTotal = CalculerFactureBT(consommation);
+                }
+                else if(rbMT.Checked)
+                {
+                    montantTotal = CalculerFactureMT(consommation);
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez selectionner une categorie de client.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string dateEmission = DateTime.Now.ToString("dd-mm-yyyy");
+                string contenuFacture = "Facture d'electricité pour " + prenomClient + " " + nomClient + " (ID: " + identifiantClient + ") émis le " + dateEmission + ":\r\n" +
+                                        "Ville : " + villeClient + "\r\n" +
+                                        "Quartier : " + quartierClient + "\r\n" +
+                                        "Consommation : " + consommation + "kwh\r\n" +
+                                        "Montent Total : " + montantTotal + "FCFA\r\n";
+                rtbFacture.Text = contenuFacture;
+            }
+            else
+            {
+                MessageBox.Show("Veuillez saisir une consommation valide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            ActiverModification();
+        }
+     
+        private void btnCharger_Click(object sender, EventArgs e)
+        {
+            ChargerFacture();
+        }
+       
+        private void btnEnregistrer_Click(object sender, EventArgs e)
+        {
+            EnregistrerFacture();
+        }
+
+        #endregion
+
+        #region Methodes Auxiliaires
+
+        private void ActiverModification()
+        {
+            txtNom.ReadOnly = false;
+            txtPrenom.ReadOnly = false;
+            txtVille.ReadOnly = false;
+            txtQuartier.ReadOnly = false;
+            txtIdentifiant.ReadOnly = false;
+            txtConsommation.ReadOnly = false;
+            rbBT.Enabled = true;
+            rbMT.Enabled = true;
+            rtbFacture.ReadOnly = false;
+
+            MessageBox.Show("Vous pouvez maintenant modifier la facture.", "Modification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void EnregistrerFacture()
+        {
+            txtNom.ReadOnly = true;
+            txtPrenom.ReadOnly = true;
+            txtVille.ReadOnly = true;
+            txtQuartier.ReadOnly = true;
+            txtIdentifiant.ReadOnly = true;
+            txtConsommation.ReadOnly = true;
+            rbBT.Enabled = false;
+            rbMT.Enabled = false;
+            rtbFacture.ReadOnly = true;
+
+            string dateEmission = DateTime.Now.ToString("dd-mm-yyyy");
+            string nomFichier = "Facture_" + txtIdentifiant.Text + "_" + dateEmission;
+            int numSequence = 1;
+            string cheminFichier = "";
+
+            do
+            {
+                cheminFichier = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nomFichier + "_" + numSequence + ".txt");
+                if (!File.Exists(cheminFichier))
+                {
+                    break;
+                }
+                numSequence++;
+            } while (true);
+            string contenuFacture = rtbFacture.Text;
+            File.WriteAllText(cheminFichier, contenuFacture);
+
+            MessageBox.Show("La facture a été enregistrée dans : " + cheminFichier, "Facture enregistrée.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ChargerFacture()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Fichiers texte (*.txt)|*.txt|Tous les fichiers(*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string contenuFacture = File.ReadAllText(openFileDialog.FileName);
+                    rtbFacture.Text = contenuFacture;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Une erreur s'est produite lors du chargement du fichier : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
